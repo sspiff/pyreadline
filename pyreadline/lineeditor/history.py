160c279b8b6f3653a85825632a455904bbc54205
sspiff <sspiff@users.noreply.github.com>
2014-10-24 20:11:14 -0400

    fix isearch result on 'next match' request

--- pyreadline/lineeditor/history.py
+++ pyreadline/lineeditor/history.py
@@ -152,8 +152,7 @@ class LineHistory(object):
         #If we get a new search without change in search term it means
         #someone pushed ctrl-r and we should find the next match
         if self.last_search_for == searchfor and startpos > 0:
-            startpos -= 1
-            for idx, line in list(enumerate(self.history))[startpos:0:-1]:
+            for idx, line in list(enumerate(self.history))[(startpos-1):0:-1]:
                 if searchfor in line:
                     startpos = idx
                     break
@@ -182,8 +181,7 @@ class LineHistory(object):
         #If we get a new search without change in search term it means
         #someone pushed ctrl-r and we should find the next match
         if self.last_search_for == searchfor and startpos < self.get_current_history_length()-1:
-            startpos += 1
-            for idx, line in list(enumerate(self.history))[startpos:]:
+            for idx, line in list(enumerate(self.history))[(startpos+1):]:
                 if searchfor in line:
                     startpos = idx
                     break
